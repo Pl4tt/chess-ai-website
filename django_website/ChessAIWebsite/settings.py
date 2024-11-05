4e3cd2895c6480302a54fbf733c86c0947432304
DESKTOP-12IV7G0\Dario <dario.patt2@gmail.com>
2024-11-05 19:03:44 +0100

    change allowed hosts

--- django_website/ChessAIWebsite/settings.py
+++ django_website/ChessAIWebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["157.230.126.55",]
+ALLOWED_HOSTS = ["157.230.126.55", "chessai-website.ch", "www.chessai-website.ch"]
 
 ROOT_URLCONF = f'{config("PROJECT_NAME")}.urls'
 
