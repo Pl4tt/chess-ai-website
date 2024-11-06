fbcf5fa37daff23d8faee402c771ccd0fb4792e6
DESKTOP-12IV7G0\Dario <dario.patt2@gmail.com>
2024-11-06 17:04:25 +0100

    bugfix apps not loaded yet

--- django_website/ChessAIWebsite/routing.py
+++ django_website/ChessAIWebsite/routing.py
@@ -2,14 +2,14 @@ import django
 import os
 from decouple import config
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
+django.setup()
+
 import chess_game.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
