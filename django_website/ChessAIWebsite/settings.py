aa98073b53498c0813aee44d08da88e8cb2da0ce
DESKTOP-12IV7G0\Dario <dario.patt2@gmail.com>
2024-11-06 17:40:54 +0100

    add redis channels

--- django_website/ChessAIWebsite/settings.py
+++ django_website/ChessAIWebsite/settings.py
@@ -159,9 +159,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-     }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
 
 
