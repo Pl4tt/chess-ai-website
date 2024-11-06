a5c2c5289547ec4f0c23d8bcdb5b78e494c598ea
DESKTOP-12IV7G0\Dario <dario.patt2@gmail.com>
2024-11-06 18:01:11 +0100

    bugfix

--- django_website/chess_game/routing.py
+++ django_website/chess_game/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chess/matchmaking/$", consumers.MatchmakingConsumer.as_consumer()),
-    re_path(r"ws/chess/(?P<game_type>\w+)/(?P<game_id>\w+)/$", consumers.ChessGameConsumer.as_consumer()),
-    re_path(r"ws/chess/abc/$", consumers.ChessGameConsumer.as_consumer()),
+    re_path(r"ws/chess/matchmaking/$", consumers.MatchmakingConsumer.as_asgi()),
+    re_path(r"ws/chess/(?P<game_type>\w+)/(?P<game_id>\w+)/$", consumers.ChessGameConsumer.as_asgi()),
+    re_path(r"ws/chess/abc/$", consumers.ChessGameConsumer.as_asgi()),
 ]
