dc5a00e7ff3bc502f97d5edba1359af5b3c860d6
Pl4tt <dario.patt2@gmail.com>
2025-05-26 02:13:20 +0200

    Add Media Storage

--- django_website/ChessAIWebsite/settings.py
+++ django_website/ChessAIWebsite/settings.py
@@ -144,8 +144,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "media"),
 ]
 
-MEDIA_URL = "/static/media/"
-
 if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
     MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
@@ -177,7 +175,9 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
 AWS_S3_CUSTOM_DOMAIN = config('AWS_S3_CUSTOM_DOMAIN')
-AWS_LOCATION = config('AWS_LOCATION')
+
+AWS_STATIC_LOCATION = config('AWS_STATIC_LOCATION')
+AWS_MEDIA_LOCATION = config('AWS_MEDIA_LOCATION')
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
@@ -191,10 +191,13 @@ STATICFILES_DIRS = [
 ]
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = f'{config("PROJECT_NAME")}.storage_backends.StaticStorage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
+
+DEFAULT_FILE_STORAGE = f'{config("PROJECT_NAME")}.storage_backends.MediaStorage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+
 TEMP = os.path.join(BASE_DIR, 'temp')
-STATICFILES_STORAGE = 'ChessAIWebsite.storage_backends.StaticStorage'
-DEFAULT_FILE_STORAGE = 'ChessAIWebsite.storage_backends.StaticStorage'
 
 
 
--- django_website/ChessAIWebsite/storage_backends.py
+++ django_website/ChessAIWebsite/storage_backends.py
@@ -2,6 +2,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.AWS_LOCATION
-    default_acl = 'public-read'
+    location = settings.AWS_STATIC_LOCATION
+    default_acl = settings.AWS_DEFAULT_ACL
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.AWS_MEDIA_LOCATION
+    default_acl = settings.AWS_DEFAULT_ACL
     custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
